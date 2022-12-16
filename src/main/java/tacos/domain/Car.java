776aea20205d474effda0a3fa6d55d89deeec483
mostafamt <mostafamt60@yahoo.com>
2022-12-16 07:02:26 +0300

    remove denpendency in class

--- src/main/java/tacos/domain/Car.java
+++ src/main/java/tacos/domain/Car.java
@@ -1,9 +1,9 @@
 package tacos.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+//import javax.persistence.Entity;
+//import javax.persistence.GeneratedValue;
+//import javax.persistence.GenerationType;
+//import javax.persistence.Id;
 
 //@Entity
 public class Car {
