a1bf81dd26b55657399ce45d6189fd7512b27d4f
mostafamt <mostafamt60@yahoo.com>
2022-12-16 04:04:07 +0300

    remove all H2 dependencies

--- src/main/java/tacos/domain/Car.java
+++ src/main/java/tacos/domain/Car.java
@@ -5,10 +5,10 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-@Entity
+//@Entity
 public class Car {
-	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+//	@Id
+//	@GeneratedValue(strategy = GenerationType.AUTO)
 	private long id;
 	private String brand, model, color, registerNumber;
 	private int year, price;
